ef6fd874e9b9f4c715ab77b8633b5fbfccfde255
Sezer Balban <109288974+thejuliusezar@users.noreply.github.com>
2026-01-19 02:38:37 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
-"""
-Ana çalışma dosyası
-Öğrenciler bu dosyayı çalıştıracaktır.
-"""
+
+from pathlib import Path 
+
+
+from src.io_utils import load_signal_csv
+from src.analysis import basic_stats, sampling_rate
+from src.signal_tools import moving_average
+from src.plotting import plot_time
+
+
+
 
 def main():
-    # TODO:
-    # 1. CSV dosyasını oku
-    # 2. örnekleme frekansını hesapla
-    # 3. temel istatistikleri yazdır
-    # 4. hareketli ortalama filtresi uygula
-    # 5. sonucu çizdir
-    pass
-
+    path = Path("data") / "sample_signal.csv"
+    
+    t, x = load_signal_csv(path)
+    
+    basic_stats(x)
+    
+    sampling_rate(t)
+    
+    moving_average(x)
+    
+    plot_time(t, x, moving_average(x))
 
 if __name__ == "__main__":
     main()
+
+    
