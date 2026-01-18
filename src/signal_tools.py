152892c921aba63786eece448b227b4dc94b8060
Sezer Balban <109288974+thejuliusezar@users.noreply.github.com>
2026-01-19 02:03:29 +0300

    Update signal_tools.py

--- src/signal_tools.py
+++ src/signal_tools.py
@@ -3,19 +3,27 @@
 Basit sinyal işleme fonksiyonları
 """
 
+import numpy as np 
+
 def moving_average(x, window_size):
-    """
-    Hareketli ortalama filtresi
+    
+    w = np.ones(window_size) / window_size
+    
+    return np.convolve(x, w, mode = "same")
 
-    Parametreler:
-        x           : sinyal vektörü
-        window_size : pencere uzunluğu (int)
 
-    Dönen:
-        y : filtrelenmiş sinyal
-    """
-    # TODO:
-    # 1. her örnek için pencereyi belirle
-    # 2. pencere içindeki ortalamayı hesapla
-    # 3. sonucu yeni bir listeye yaz
-    pass
+""" moving_average fonksiyonunu kendim de yaptım fakat grafik çizme noktasında sıkıntılar yarattığı için direk numphy ile yapma kararı aldım.  """
+
+
+# def moving_average(x, window_size = 10):
+    
+#     n = len(x)
+    
+#     hareketli_ortalama = []
+#     for i in range(n - window_size + 1):
+#         pencere_toplam = 0.0
+#         for j in range(i, i + window_size):
+#             pencere_toplam += x[j]
+#         hareketli_ortalama.append(pencere_toplam / window_size)
+
+#     return hareketli_ortalama
